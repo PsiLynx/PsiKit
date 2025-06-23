25d61917c2f4c12b6a04b2fe80921a8ca49667d6
AS1624 <thebest1dudes@gmail.com>
2025-06-22 17:40:59 -0800

    add a bunch of cpp files, etc.

--- native/src/main/cpp/enumerate.py
+++ native/src/main/cpp/enumerate.py
@@ -6,22 +6,23 @@ def enumerateFolder(folderName: str, action: Callable[[str, str], None]):
     for entry in os.listdir(folderName):
         print(entry)
         if os.path.isfile(folderName + "/" + entry):
-            print(True)
             action(folderName, entry)
         else:
             enumerateFolder(folderName + "/" + entry, action)
 
 wpiFiles: list[str] = []
-wpiDir = "/home/Avery/Documents/allwpilib/wpiutil/src/main/native"
+wpiDir = "/home/Avery/Documents/allwpilib/wpiutil/src/main"
 
 def addWPIFile(folderName, fileName):
     if fileName[-3:] == 'cpp':
-        wpiFiles.append(folderName.strip(wpiDir) + "/" + fileName) 
+        wpiFiles.append(folderName + "/" + fileName) 
 
 def moveWPIFile(folderName, fileName):
+    fileName = fileName[0:-1] + "cpp"
+    print(fileName)
     candidates: list[str] = []
     for file in wpiFiles:
-        if file.endswith(fileName):
+        if file.endswith("/"+fileName):
             candidates.append(file)
     wholeTarget = folderName + "/" + fileName
     if os.path.exists(wholeTarget):
